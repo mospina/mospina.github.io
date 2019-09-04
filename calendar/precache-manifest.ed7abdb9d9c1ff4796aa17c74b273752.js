10b671c006ea8f6c006fa0e7e2cffdb41afd0038
Manuel Ospina <manuel@anect.jp>
2019-09-04 12:19:00 +0900

    Add calendar app

---
+++ calendar/precache-manifest.ed7abdb9d9c1ff4796aa17c74b273752.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df07d3f637e7cc502aaba13f460f9c9",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "daa99cf11e296b2aa8df",
+    "url": "/calendar/static/css/main.737861bf.chunk.css"
+  },
+  {
+    "revision": "0be68c4b22185c2dbbbb",
+    "url": "/calendar/static/js/2.a46c86cb.chunk.js"
+  },
+  {
+    "revision": "daa99cf11e296b2aa8df",
+    "url": "/calendar/static/js/main.69bc91ae.chunk.js"
+  },
+  {
+    "revision": "c4ff9231f949b1bbda3a",
+    "url": "/calendar/static/js/runtime~main.0646e76b.js"
+  }
+]);
